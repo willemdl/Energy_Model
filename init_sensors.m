f001c34824720d5abff904264d92b1e4bd30b89d
Willem de Laat <willem.delaat97@gmail.com>
2020-05-08 13:30:00 +0200

    Made .m files for the main code, added comments to different codes

---
+++ init_sensors.m
@@ -0,0 +1,15 @@
+
+%Implemented as an function because that gives more insight in which
+%parameters are defined and their names compared to a normal matlab script.
+%the output of this function should be 1 big matrix, each column is a
+%different sensor, each row is a different parameter. The following
+%parameters need to be defined:
+% 1) The name of the sensor.
+% 2) Voltage at which the sensor operates.
+% 3) current drawn during measurement.
+% 4) Time it takes to do 1 measurement.
+
+
+%optionally:
+% 1) the type of sensor.
+% 2) accuracy? (stel dat je uiteindelijk 
