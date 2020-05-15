9ded15faca33811f5559747ccbba8c7be65defc1
Willem de Laat <willem.delaat97@gmail.com>
2020-05-15 12:52:19 +0200

    power and energy insights per sensor etc and per stage, improved disp_totalenergy

--- disp_totalenergy.m
+++ disp_totalenergy.m
@@ -1,4 +1,4 @@
-function [] = disp_totalenergy(P_Total, E_Total, T_Array)
+function [] = disp_totalenergy(P_Sub, P_Total, E_Total, T_Array, tmeasurement)
 disp('started disp_totalenergy');
 
 figure();
@@ -16,13 +16,47 @@ xlabel('Time');
 xlim([0 max(T_Array(:))]);
 ylabel('P [mW]');
 legend('Power');
-disp('finished disp_totalenergy');
 
 figure();
-plot(T_Array(1:end-1),diff(T_Array),'-x'); %https://nl.mathworks.com/help/physmod/simscape/ug/determine-step-size.html
+plot(T_Array(:), P_Sub(:,5));
+title('Power drawn2');
+xlabel('Time');
+xlim([0 max(T_Array(:))]);
+ylabel('P [mW]');
+legend('Power');
+
+
+figure();
+semilogy(T_Array(1:end-1),diff(T_Array),'-x'); %https://nl.mathworks.com/help/physmod/simscape/ug/determine-step-size.html
 title('Step size of totalenergy');
 xlabel('Time');
 xlim([0 max(T_Array(:))]);
 ylabel('Step size [s]');
+
+
+
+%---------- uitzoeken welke combinaties van intervallen mogelijk zijn,
+%stel: intervallen 300 400 en 500 dan heb je deze 3 verschillende metingen
+%maar ook de combinaties van deze intervallen. als we de tijden kunnen
+%bepalen wanneer deze combinaties voor het eerst plaats vinden dan kunnen
+%we al deze mogelijkheden bijelkaar/naastelkaar plotten. 
+M_Plot = 1;
+test = (tmeasurement(M_Plot,2)-tmeasurement(M_Plot,1))/2;
+figure();
+plot(T_Array(:), P_Total(:));
+xlim([(tmeasurement(M_Plot,1)-test) (tmeasurement(M_Plot,2)+test)]);
+title('Power drawn measuremnt1');
+xlabel('Time');
+ylabel('P [mW]');
+legend('Power');
+disp('finished disp_totalenergy');
+
+figure();
+plot(T_Array(:), P_Sub(:,6));
+xlim([(tmeasurement(M_Plot,1)-test) (tmeasurement(M_Plot,2)+test)]);
+title('Stages measuremnt1');
+xlabel('Time');
+ylabel('Stage');
+legend('stages');
 disp('finished disp_totalenergy');
 end
--- main.m
+++ main.m
@@ -14,16 +14,17 @@ S_MCU = MCU(:,1);
 S_Com = Transmission(:,1);
 S_names = [Sensorname(1,1) Sensorname(1,3) MCUname(1,1) Transmissionname(1,1)];
 I_Array = [ (3600) 1 0 1 0; (3*3600) 1 1 1 1];
-
+T_Max = 3*24*3600;
+E_Max = 1000;
 %I_Array first column = interval time in [s]
 % second column = sensor 1, third column is sensor 2, etc untill all
 % sensors have been defined to be on (1) or off(0) than follows the MCU and 
 % lastly the communication. Make sure the order of sensors is of the same
 % as the matrix S_Sensors.
-[P_Total, E_Total, T_Total, measurements2, time,tmeasurement, tltest] = calc_totalenergy(S_Sensors, S_MCU, S_Com, I_Array);
+[ P_Sub, E_Sub, P_Total, E_Total, measurements2, time,tmeasurement, tltest] = calc_totalenergy(S_Sensors, S_MCU, S_Com, I_Array, T_Max, E_Max);
 
 %% invoke plots and other graphical results
 %disp_sensors(Results_calc_sensors, Sensorname);
-disp_totalenergy(P_Total, E_Total, time);
+disp_totalenergy(P_Sub, P_Total, E_Total, time, tmeasurement);
 
 
